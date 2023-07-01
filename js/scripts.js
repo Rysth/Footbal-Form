a8200c00dc211d9a68f9daa37adec7752659b40b
Rysth <johnpalacios.t@gmail.com>
2023-07-01 13:48:23 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,16 @@
-function addSelectionState(array) {
-  array.forEach((element) => {
-    element.addEventListener('click', () => {
-      array.forEach((component) => {
-        const isComponentSelected = component.classList.contains('selected');
-        component.classList.toggle('selected', !isComponentSelected);
-        component.classList.toggle('unselected', isComponentSelected);
+function addSelectionState(...courtIds) {
+  courtIds.forEach((courtId) => {
+    const court = Array.from(document.querySelectorAll(`#${courtId}`));
+    court.forEach((element) => {
+      element.addEventListener('click', () => {
+        court.forEach((component) => {
+          const isComponentSelected = component.classList.contains('selected');
+          component.classList.toggle('selected', !isComponentSelected);
+          component.classList.toggle('unselected', isComponentSelected);
+        });
       });
     });
   });
 }
 
-/* Front & Back Side */
-const topCourt = Array.from(document.querySelectorAll('#top'));
-const bottomCourt = Array.from(document.querySelectorAll('#bottom'));
-const rightCourt = Array.from(document.querySelectorAll('#right'));
-const leftCourt = Array.from(document.querySelectorAll('#left'));
-
-addSelectionState(topCourt);
-addSelectionState(bottomCourt);
-addSelectionState(rightCourt);
-addSelectionState(leftCourt);
+addSelectionState('top', 'bottom', 'right', 'left');
