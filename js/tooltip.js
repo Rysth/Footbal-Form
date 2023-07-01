0616b7de879faf0e39fafc73511bbef341ee75bf
Rysth <johnpalacios.t@gmail.com>
2023-07-01 13:46:03 -0500

    Added tooltip.js

---
+++ js/tooltip.js
@@ -0,0 +1,18 @@
+tippy('#top', {
+  content: 'Superior',
+});
+
+tippy('#bottom', {
+  content: 'Inferior',
+  placement: 'bottom',
+});
+
+tippy('#right', {
+  content: 'Derecha',
+  placement: 'right',
+});
+
+tippy('#left', {
+  content: 'Izquierda',
+  placement: 'left',
+});
